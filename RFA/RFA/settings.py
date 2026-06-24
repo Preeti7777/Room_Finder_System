64d720d0bf3f7f166662789ce8465c180c6ac611
Preeti Gurung <gurungpreeti999@gmail.com>
2026-06-24 13:11:38 +0545

    Update ALLOWED_HOSTS in settings.py to include localhost and production domain

--- RFA/RFA/settings.py
+++ RFA/RFA/settings.py
@@ -31,7 +31,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "ghar-khoj.onrender.com",
+]
 
 SECRET_KEY = config('SECRET_KEY')
 # Application definition
